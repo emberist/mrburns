34e8f1f18ef590cb281b65dcbd41450f2384e087
Riccardo Caranfil <13602797+emberist@users.noreply.github.com>
2024-05-21 15:32:04 +0200

    feat: add new branch prefixes from config files

--- src/commands/wizard.rs
+++ src/commands/wizard.rs
@@ -1,15 +1,18 @@
 use cliclack::{
-    confirm,
+    confirm, input,
     log::{self, info},
 };
 
-use crate::{cli::ConfigArgs, config::Config};
+use crate::{
+    cli::ConfigArgs,
+    config::{BranchPrefixes, Config},
+};
 
 pub fn start_config_wizard(args: &ConfigArgs) -> anyhow::Result<()> {
     info("Configuration started")?;
 
     if args.get {
-        let config = Config::read()?;
+        let config = Config::read();
 
         log::success(format!(
             "Here your config: {}",
@@ -40,7 +43,33 @@ pub fn start_config_wizard(args: &ConfigArgs) -> anyhow::Result<()> {
     let create_draft_mr =
         confirm("Do you want the merge request created to be draft as default?").interact()?;
 
-    Config::write(Config { create_draft_mr })?;
+    let default_config = Config::default();
+
+    let feature = input("Which feature branch prefix would you like to use?")
+        .default_input(&default_config.branch_prefixes.feature)
+        .interact()?;
+
+    let chore = input("Which chore branch prefix would you like to use?")
+        .default_input(&default_config.branch_prefixes.chore)
+        .interact()?;
+
+    let bugfix = input("Which bugfix branch prefix would you like to use?")
+        .default_input(&default_config.branch_prefixes.bugfix)
+        .interact()?;
+
+    let release = input("Which release branch prefix would you like to use?")
+        .default_input(&default_config.branch_prefixes.release)
+        .interact()?;
+
+    Config::write(Config {
+        create_draft_mr,
+        branch_prefixes: BranchPrefixes {
+            bugfix,
+            chore,
+            feature,
+            release,
+        },
+    })?;
 
     Ok(())
 }
